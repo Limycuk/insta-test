f831e1c5ac7465db707d0f40e9bdc1a590cb92ca
Baltsevich Alexander <baltsevicha@gmail.com>
2018-12-26 21:25:46 +0300

    Updates

--- precache-manifest.a3893090fc6472c5c2ea21d0ba347974.js
+++ precache-manifest.a3893090fc6472c5c2ea21d0ba347974.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/insta-test/static/js/runtime~main.07e7666e.js"
   },
   {
-    "revision": "0d95459664c98ce7251d",
-    "url": "/insta-test/static/js/main.0d954596.chunk.js"
+    "revision": "c9506ebfaa6db55b8f44",
+    "url": "/insta-test/static/js/main.c9506ebf.chunk.js"
   },
   {
     "revision": "27ed019165fc47f66a98",
     "url": "/insta-test/static/js/1.27ed0191.chunk.js"
   },
   {
-    "revision": "0d95459664c98ce7251d",
+    "revision": "c9506ebfaa6db55b8f44",
     "url": "/insta-test/static/css/main.8519d0e5.chunk.css"
   },
   {
-    "revision": "f6a2cdcb541fb0eb7fc1fd0b8a0c59e8",
+    "revision": "12cea3ea5a5eae38ee2e4eb90dfafc3a",
     "url": "/insta-test/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/insta-test/precache-manifest.c093089b9661a0024f81c492be8fb24c.js"
+  "/insta-test/precache-manifest.a3893090fc6472c5c2ea21d0ba347974.js"
 );
 
 workbox.clientsClaim();
