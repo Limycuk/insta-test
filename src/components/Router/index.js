e3f9a00366eadccd23cee5146d2fe09b27188815
Baltsevich Alexander <baltsevicha@gmail.com>
2019-01-05 04:03:45 +0300

    fixed eslint errors

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,14 +1,3 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
 import View from './view';
 
-class Router extends Component {
-  render() {
-    const props = {};
-
-    return <View {...props} />;
-  }
-}
-
-export default Router;
+export default View;
--- src/pages/Home/components/Home/index.js
+++ src/pages/Home/components/Home/index.js
@@ -1,14 +1,3 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
 import View from './view';
 
-class HomeContainer extends Component {
-  render() {
-    const props = {};
-
-    return <View {...props} />;
-  }
-}
-
-export default HomeContainer;
+export default View;
